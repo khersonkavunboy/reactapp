64d2484af166f30dbf37636e37e8dbfe026a6d69
khersonkavunboy <57249060+khersonkavunboy@users.noreply.github.com>
2021-01-25 18:16:02 +0200

    LastWorkVersion

--- src/components/PagesViewProviderOrClient/PagesViewProvider.jsx
+++ src/components/PagesViewProviderOrClient/PagesViewProvider.jsx
@@ -1,9 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import "./PagesViewProvider.css"
-import axios from 'axios';
 import DataTable from '../GridTable/GridTable';
 import Button from "@material-ui/core/Button";
-import {CellParams} from "@material-ui/data-grid";
 
 const rows = [
     { id: 1, fullName: 'Snow Snow', numberPhone: '382282842871', dateTime: Date.now(), service:"Боулинг", duration:'1 hours' },
@@ -21,7 +19,7 @@ const columns = [
     { field: 'service', headerName: 'Service', type: 'text', width: 200 },
     { field: 'duration', headerName: 'Duration', type: 'text', width: 200 },
     { field: "", headerName: "Function", sortable: false, width: 200, disableClickEventBubbling: true,
-        renderCell:(params: CellParams) => {
+        renderCell:({params}) => {
             const onClick = () => {
                 const fields = params.api.getAllColumns().map(c => c.field);
                 const thisRow = {};
@@ -32,18 +30,8 @@ const columns = [
         }
     }
 ];
-  
-
-
 
 export const PagesViewProvider = () => {
-    // const [providers, setProviders] = useState([]);
-    // useEffect(() => loadProviders(), []);
-    // const loadProviders = async () => {
-    //     const result = await axios.get("http://localhost:3030/providers");
-    //     setProviders(result.data)
-    // };
-
     return (
         <>
             <div className="textStyle"><h1>Provider dashboard</h1></div>
